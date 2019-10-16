290f00403a4d98ce2e9ac7b7447618de458528ba
anshuman-lab <56531419+anshuman-lab@users.noreply.github.com>
2019-10-16 15:35:22 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+
+        System.out.print("Enter second number: ");
+        int num2 = scan.nextInt();
+
+        System.out.println("Output: "+product);
+    }
+}
